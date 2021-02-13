bb6551b6059eacdf59763ea193be4e1eac09c616
Victor Berchet <victor@suumit.com>
2021-02-13 13:41:06 -0800

    Simplify the chart component

--- frontend/src/viewer/components/chart-element.ts
+++ frontend/src/viewer/components/chart-element.ts
@@ -70,6 +70,7 @@ export class ChartElement extends connect(store)(LitElement) {
   // Throttle timestamp and airspace updates.
   private nextAspUpdate = 0;
   private nextTimestampUpdate = 0;
+  private sizeListener = () => this.updateSize();
 
   stateChanged(state: RootState): void {
     this.tracks = sel.tracks(state);
@@ -131,11 +132,6 @@ export class ChartElement extends connect(store)(LitElement) {
     }
   }
 
-  connectedCallback(): void {
-    super.connectedCallback();
-    window.addEventListener('resize', () => this.updateSize());
-  }
-
   static get styles(): CSSResult[] {
     return [
       css`
@@ -147,8 +143,6 @@ export class ChartElement extends connect(store)(LitElement) {
           font: 12px 'Nobile', verdana, sans-serif;
         }
         #chart {
-          width: 100%;
-          height: 100%;
           touch-action: none;
         }
         .paths {
@@ -247,248 +241,29 @@ export class ChartElement extends connect(store)(LitElement) {
     ];
   }
 
-  private paths(): TemplateResult[] {
-    const paths: TemplateResult[] = [];
-
-    // Dot not render before the width is set.
-    if (this.tracks.length == 0 || this.width < 50) {
-      return paths;
-    }
-
-    const spanTs = this.maxTs - this.minTs;
-    const scaleY = -this.height / (this.maxY - this.minY);
-    const offsetY = -this.maxY * scaleY;
-    let activePath: SVGTemplateResult | undefined;
-
-    // Display the gnd elevation only if there is a single track & mode is altitude
-
-    const displayGndAlt = this.tracks.length == 1 && this.chartYAxis == ChartYAxis.Altitude;
-
-    this.tracks.forEach((track) => {
-      if (track.ts.length < 5) {
-        return;
-      }
-      // Span of the track on the X axis.
-      const tsOffset = this.tsOffsets[track.id];
-      const minX = Math.round(((track.ts[0] - tsOffset - this.minTs) / spanTs) * this.width);
-      const maxX = Math.round(((track.ts[track.ts.length - 1] - tsOffset - this.minTs) / spanTs) * this.width);
-      const spanX = maxX - minX;
-      const spanTrackTs = track.maxTs - track.minTs;
-
-      const trackCoords: string[] = [];
-      const gndCoords = [`${minX},${(this.minY * scaleY + offsetY).toFixed(1)}`];
-
-      if (displayGndAlt && track.gndAlt) {
-        paths.push(...this.airspacePaths(track, minX, spanX, spanTrackTs, scaleY, offsetY));
-      }
-      for (let x = minX; x < maxX; x++) {
-        const ts = ((x - minX) / spanX) * spanTrackTs + track.minTs;
-        const y = this.getY(track, ts);
-        trackCoords.push(`${x},${(y * scaleY + offsetY).toFixed(1)}`);
-        if (displayGndAlt && track.gndAlt) {
-          const gndAlt = sampleAt(track.ts, track.gndAlt, ts);
-          gndCoords.push(`${x},${(gndAlt * scaleY + offsetY).toFixed(1)}`);
-        }
-      }
-      gndCoords.push(`${maxX},${(this.minY * scaleY + offsetY).toFixed(1)}`);
-      if (displayGndAlt) {
-        paths.push(svg`<path class=gnd d=${`M${gndCoords.join('L')}`}></path>`);
-      }
-      if (track.id == this.currentTrackId) {
-        activePath = svg`<path class='active' stroke=${this.trackColors[track.id]} filter=url(#shadow-active) 
-          d=${`M${trackCoords.join('L')}`}></path>`;
-      } else {
-        paths.push(
-          svg`<path stroke=${this.trackColors[track.id]} d=${`M${trackCoords.join('L')}`} filter=url(#shadow)></path>`,
-        );
-      }
-    });
-
-    // The active path should be drawn last to be on top of others.
-    if (activePath) {
-      paths.push(activePath);
-    }
-
-    return paths;
+  connectedCallback(): void {
+    super.connectedCallback();
+    window.addEventListener('resize', this.sizeListener);
   }
 
-  // Compute the SVG paths for the airspaces.
-  private airspacePaths(
-    track: RuntimeTrack,
-    minX: number,
-    spanX: number,
-    spanTrackTs: number,
-    scaleY: number,
-    offsetY: number,
-  ): SVGTemplateResult[] {
-    const asp = track.airspaces;
-    if (asp == null) {
-      return [];
-    }
-    const paths: SVGTemplateResult[] = [];
-
-    for (let i = 0; i < asp.startTs.length; i++) {
-      const flags = asp.flags[i];
-      if (!this.showRestricted && flags & Flags.AirspaceRestricted) {
-        continue;
-      }
-      const start = asp.startTs[i];
-      const end = asp.endTs[i];
-      const top = asp.top[i];
-      const bottom = asp.bottom[i];
-      const topRefGnd = flags & Flags.TopRefGnd;
-      const bottomRefGnd = flags & Flags.FloorRefGnd;
-      const clampTo: { minAlt?: number; maxAlt?: number } = {};
-      if (bottomRefGnd && !topRefGnd) {
-        clampTo.maxAlt = top;
-      }
-      if (topRefGnd && !bottomRefGnd) {
-        clampTo.minAlt = bottom;
-      }
-      const coords = [
-        ...this.aspLine(track, start, end, minX, spanX, spanTrackTs, bottom, bottomRefGnd, clampTo),
-        ...this.aspLine(track, end, start, minX, spanX, spanTrackTs, top, topRefGnd, clampTo),
-      ];
-      if (coords.length < 4) {
-        continue;
-      }
-      coords.push(coords[0]);
-      const path = coords
-        .map(([time, alt]) => {
-          const x = ((time - track.minTs) / spanTrackTs) * spanX + minX;
-          const y = alt * scaleY + offsetY;
-          return `${x.toFixed(1)},${y.toFixed(1)}`;
-        })
-        .join('L');
-      paths.push(
-        svg`<path data-start=${start} data-end=${end} title=${`[${asp.category[i]}] ${asp.name[i]}`}  class=${`asp ${airspaceCategory(
-          flags,
-        )}`} d=${'M' + path}></path>`,
-      );
-    }
-
-    return paths;
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+    window.removeEventListener('resize', this.sizeListener);
   }
 
-  private aspLine(
-    track: RuntimeTrack,
-    start: number,
-    end: number,
-    minX: number,
-    spanX: number,
-    spanTrackTs: number,
-    alt: number,
-    refGnd: number,
-    clampTo: { minAlt?: number; maxAlt?: number },
-  ): Array<[number, number]> {
-    if (!refGnd || !track.gndAlt) {
-      return [
-        [start, alt],
-        [end, alt],
-      ];
-    }
-    let reverse = false;
-    if (start > end) {
-      [start, end] = [end, start];
-      reverse = true;
-    }
-    const startX = Math.round(((start - track.minTs) / spanTrackTs) * spanX + minX);
-    const endX = Math.round(((end - track.minTs) / spanTrackTs) * spanX + minX);
-    const points: Array<[number, number]> = [];
-    const { minAlt, maxAlt } = clampTo;
-    for (let x = startX; x < endX; x++) {
-      const ts = ((x - minX) / spanX) * spanTrackTs + track.minTs;
-      const gndAlt = sampleAt(track.ts, track.gndAlt, ts);
-      let altitude = alt + gndAlt;
-      if (maxAlt != null) {
-        altitude = Math.min(maxAlt, altitude);
+  shouldUpdate(changedProps: PropertyValues): boolean {
+    if (changedProps.has('timestamp')) {
+      if (this.thumbElement) {
+        const x = String(this.getXAtTimestamp(this.timestamp));
+        this.thumbElement.setAttribute('x1', x);
+        this.thumbElement.setAttribute('x2', x);
       }
-      if (minAlt != null) {
-        altitude = Math.max(minAlt, altitude);
-      }
-      points.push([ts, altitude]);
+      changedProps.delete('timestamp');
     }
-    return reverse ? points.reverse() : points;
-  }
-
-  private axis(): TemplateResult[] {
-    const axis: TemplateResult[] = [];
-
-    if (this.tracks) {
-      const tks = ticks(this.minY, this.maxY, 4);
-      const ySpan = this.maxY - this.minY;
-
-      tks.forEach((tick) => {
-        // Draw line
-        const y = ((this.maxY - tick) / ySpan) * this.height;
-        axis.push(svg`<line y1=${y.toFixed(1)} x2=${this.width} y2=${y}></line>`);
-      });
-    }
-
-    return axis;
-  }
-
-  private yTexts(): TemplateResult[] {
-    const texts: TemplateResult[] = [];
-
-    if (this.tracks) {
-      const tks = ticks(this.minY, this.maxY, 4);
-      const ySpan = this.maxY - this.minY;
-
-      tks.forEach((tick) => {
-        const y = ((this.maxY - tick) / ySpan) * this.height;
-        texts.push(
-          svg`<text stroke-width=3 x=5 y=${y.toFixed(1)} dy=-2>${formatUnit(tick, this.getYUnit())}</text>
-          <text x=5 y=${y.toFixed(1)} dy=-2>${formatUnit(tick, this.getYUnit())}</text>`,
-        );
-      });
-    }
-
-    return texts;
-  }
-
-  private xTexts(): TemplateResult[] {
-    const texts: TemplateResult[] = [];
-
-    if (this.tracks) {
-      const minute = 60 * 1000;
-      const hour = 60 * minute;
-
-      // Push minTs 50px right to avoid writing over the alt scale
-      const offset = 50;
-      const minTs = this.minTs + ((this.maxTs - this.minTs) / this.width) * offset;
-
-      const timeSpan = this.maxTs - minTs;
-
-      const tickSpan = Math.ceil(timeSpan / hour / 6) * hour;
-      const time = new Date(minTs);
-      const startTs = new Date(time.getFullYear(), time.getMonth(), time.getDate(), time.getHours() + 1).getTime();
-
-      for (let ts = startTs; ts < this.maxTs; ts += tickSpan) {
-        const x = ((ts - this.minTs) / (this.maxTs - this.minTs)) * this.width;
-        const time = new Date(ts).toLocaleTimeString();
-        texts.push(
-          svg`<text text-anchor=middle stroke-width=3 y=${this.height} x=${x} dy=-4>${time}</text>
-          <text text-anchor=middle y=${this.height} x=${x} dy=-4>${time}</text>`,
-        );
-      }
-    }
-
-    return texts;
-  }
-
-  private updateSize(): void {
-    const shadowRoot = this.shadowRoot as ShadowRoot;
-    const host = shadowRoot.host;
-    this.width = host.clientWidth;
-    this.height = host.clientHeight;
+    return super.shouldUpdate(changedProps);
   }
 
   protected render(): TemplateResult {
-    if (!this.width) {
-      // FF and Edge would report a size of 0x0 without setTimeout
-      setTimeout(() => this.updateSize(), 0);
-    }
     return html`
       <link
         rel="stylesheet"
@@ -496,9 +271,12 @@ export class ChartElement extends connect(store)(LitElement) {
       />
       <svg
         id="chart"
+        xmlns="http://www.w3.org/2000/svg"
         @pointermove=${this.handlePointerMove}
         @pointerdown=${this.handlePointerDown}
         @wheel=${this.handleMouseWheel}
+        width=${this.width}
+        height=${this.height}
       >
         <defs>
           <filter id="shadow-active">
@@ -547,6 +325,237 @@ export class ChartElement extends connect(store)(LitElement) {
     `;
   }
 
+  protected firstUpdated(): void {
+    // Wait for the element to get a size.
+    // Then `updateSize()` will trigger a re-render by updating properties.
+    // It helps with Safari which needs explicit width and height.
+    // See
+    const timeout = Date.now() + 5000;
+    const waitForSize = () => {
+      if (this.clientWidth > 0) {
+        this.updateSize();
+      } else if (Date.now() < timeout) {
+        setTimeout(waitForSize, 50);
+      }
+    };
+    waitForSize();
+  }
+
+  private paths(): TemplateResult[] {
+    const paths: TemplateResult[] = [];
+
+    // Dot not render before the width is set.
+    if (this.tracks.length == 0 || this.width < 50) {
+      return paths;
+    }
+
+    let activePath: SVGTemplateResult | undefined;
+
+    // Display the gnd elevation only if there is a single track & mode is altitude
+
+    const displayGndAlt = this.tracks.length == 1 && this.chartYAxis == ChartYAxis.Altitude;
+
+    this.tracks.forEach((track) => {
+      if (track.ts.length < 5) {
+        return;
+      }
+      // Span of the track on the X axis.
+      const tsOffset = this.tsOffsets[track.id];
+      const minX = this.getXAtTimestamp(track.ts[0], tsOffset);
+      const maxX = this.getXAtTimestamp(track.ts[track.ts.length - 1], tsOffset);
+
+      const trackCoords: string[] = [];
+      const gndCoords = [`${minX},${this.getYAtHeight(this.minY).toFixed(1)}`];
+
+      if (displayGndAlt && track.gndAlt) {
+        paths.push(...this.airspacePaths(track));
+      }
+      for (let x = minX; x < maxX; x++) {
+        const ts = this.getTimestampAtX(x);
+        const y = this.getY(track, ts);
+        trackCoords.push(`${x.toFixed(1)},${this.getYAtHeight(y).toFixed(1)}`);
+        if (displayGndAlt && track.gndAlt) {
+          const gndAlt = sampleAt(track.ts, track.gndAlt, ts);
+          gndCoords.push(`${x.toFixed(1)},${this.getYAtHeight(gndAlt).toFixed(1)}`);
+        }
+      }
+      gndCoords.push(`${maxX},${this.getYAtHeight(this.minY).toFixed(1)}`);
+      if (displayGndAlt) {
+        paths.push(svg`<path class=gnd d=${`M${gndCoords.join('L')}`}></path>`);
+      }
+      if (track.id == this.currentTrackId) {
+        activePath = svg`<path class='active' stroke=${this.trackColors[track.id]} filter=url(#shadow-active) 
+          d=${`M${trackCoords.join('L')}`}></path>`;
+      } else {
+        paths.push(
+          svg`<path stroke=${this.trackColors[track.id]} d=${`M${trackCoords.join('L')}`} filter=url(#shadow)></path>`,
+        );
+      }
+    });
+
+    // The active path should be drawn last to be on top of others.
+    if (activePath) {
+      paths.push(activePath);
+    }
+
+    return paths;
+  }
+
+  // Compute the SVG paths for the airspaces.
+  private airspacePaths(track: RuntimeTrack): SVGTemplateResult[] {
+    const asp = track.airspaces;
+    if (asp == null) {
+      return [];
+    }
+    const paths: SVGTemplateResult[] = [];
+
+    for (let i = 0; i < asp.startTs.length; i++) {
+      const flags = asp.flags[i];
+      if (!this.showRestricted && flags & Flags.AirspaceRestricted) {
+        continue;
+      }
+      const start = asp.startTs[i];
+      const end = asp.endTs[i];
+      const top = asp.top[i];
+      const bottom = asp.bottom[i];
+      const topRefGnd = flags & Flags.TopRefGnd;
+      const bottomRefGnd = flags & Flags.FloorRefGnd;
+      const clampTo: { minAlt?: number; maxAlt?: number } = {};
+      if (bottomRefGnd && !topRefGnd) {
+        clampTo.maxAlt = top;
+      }
+      if (topRefGnd && !bottomRefGnd) {
+        clampTo.minAlt = bottom;
+      }
+      const coords = [
+        ...this.aspLine(track, start, end, bottom, bottomRefGnd, clampTo),
+        ...this.aspLine(track, end, start, top, topRefGnd, clampTo),
+      ];
+      if (coords.length < 4) {
+        continue;
+      }
+      coords.push(coords[0]);
+      const path = coords
+        .map(([time, alt]) => {
+          const x = this.getXAtTimestamp(time);
+          const y = this.getYAtHeight(alt);
+          return `${x.toFixed(1)},${y.toFixed(1)}`;
+        })
+        .join('L');
+      paths.push(
+        svg`<path data-start=${start} data-end=${end} title=${`[${asp.category[i]}] ${asp.name[i]}`}  class=${`asp ${airspaceCategory(
+          flags,
+        )}`} d=${'M' + path}></path>`,
+      );
+    }
+
+    return paths;
+  }
+
+  private aspLine(
+    track: RuntimeTrack,
+    start: number,
+    end: number,
+    alt: number,
+    refGnd: number,
+    clampTo: { minAlt?: number; maxAlt?: number },
+  ): Array<[number, number]> {
+    if (!refGnd || !track.gndAlt) {
+      return [
+        [start, alt],
+        [end, alt],
+      ];
+    }
+    let reverse = false;
+    if (start > end) {
+      [start, end] = [end, start];
+      reverse = true;
+    }
+    const startX = this.getXAtTimestamp(start);
+    const endX = this.getXAtTimestamp(end);
+    const points: Array<[number, number]> = [];
+    const { minAlt, maxAlt } = clampTo;
+    for (let x = startX; x < endX; x++) {
+      const ts = this.getTimestampAtX(x);
+      const gndAlt = sampleAt(track.ts, track.gndAlt, ts);
+      let altitude = alt + gndAlt;
+      if (maxAlt != null) {
+        altitude = Math.min(maxAlt, altitude);
+      }
+      if (minAlt != null) {
+        altitude = Math.max(minAlt, altitude);
+      }
+      points.push([ts, altitude]);
+    }
+    return reverse ? points.reverse() : points;
+  }
+
+  private axis(): TemplateResult[] {
+    const axis: TemplateResult[] = [];
+
+    if (this.tracks) {
+      const tks = ticks(this.minY, this.maxY, 4);
+
+      tks.forEach((tick) => {
+        // Draw line
+        const y = this.getYAtHeight(tick);
+        axis.push(svg`<line y1=${y.toFixed(1)} x2=${this.width} y2=${y}></line>`);
+      });
+    }
+
+    return axis;
+  }
+
+  private yTexts(): TemplateResult[] {
+    const texts: TemplateResult[] = [];
+
+    if (this.tracks) {
+      const tks = ticks(this.minY, this.maxY, 4);
+
+      tks.forEach((tick) => {
+        const y = this.getYAtHeight(tick);
+        texts.push(
+          svg`<text stroke-width=3 x=5 y=${y.toFixed(1)} dy=-2>${formatUnit(tick, this.getYUnit())}</text>
+          <text x=5 y=${y.toFixed(1)} dy=-2>${formatUnit(tick, this.getYUnit())}</text>`,
+        );
+      });
+    }
+
+    return texts;
+  }
+
+  private xTexts(): TemplateResult[] {
+    const texts: TemplateResult[] = [];
+
+    if (this.tracks) {
+      const minute = 60 * 1000;
+      const hour = 60 * minute;
+
+      // Push minTs 50px right to avoid writing over the alt scale
+      const minTs = this.getTimestampAtX(50);
+      const timeSpan = this.maxTs - minTs;
+      const tickSpan = Math.ceil(timeSpan / hour / 6) * hour;
+      const time = new Date(minTs);
+      const startTs = new Date(time.getFullYear(), time.getMonth(), time.getDate(), time.getHours() + 1).getTime();
+
+      for (let ts = startTs; ts < this.maxTs; ts += tickSpan) {
+        const x = this.getXAtTimestamp(ts);
+        const time = new Date(ts).toLocaleTimeString();
+        texts.push(
+          svg`<text text-anchor=middle stroke-width=3 y=${this.height} x=${x.toFixed(1)} dy=-4>${time}</text>
+          <text text-anchor=middle y=${this.height} x=${x.toFixed(1)} dy=-4>${time}</text>`,
+        );
+      }
+    }
+
+    return texts;
+  }
+
+  private updateSize(): void {
+    this.width = this.clientWidth;
+    this.height = this.clientHeight;
+  }
+
   private handlePlay() {
     if (this.playTimer) {
       clearInterval(this.playTimer);
@@ -577,8 +586,16 @@ export class ChartElement extends connect(store)(LitElement) {
     store.dispatch(setChartYAxis(y));
   }
 
-  private xForTimestamp(): number {
-    return Math.round(((this.timestamp - this.minTs) / (this.maxTs - this.minTs)) * this.width);
+  private getXAtTimestamp(timestamp: number, tsOffset = 0): number {
+    return Math.round(((timestamp - tsOffset - this.minTs) / (this.maxTs - this.minTs)) * this.width);
+  }
+
+  private getTimestampAtX(x: number) {
+    return (x / this.width) * (this.maxTs - this.minTs) + this.minTs;
+  }
+
+  private getYAtHeight(height: number) {
+    return ((this.maxY - height) / (this.maxY - this.minY)) * this.height;
   }
 
   private handlePointerDown(e: MouseEvent): void {
@@ -632,17 +649,5 @@ export class ChartElement extends connect(store)(LitElement) {
       });
     }
     store.dispatch(setAirspacesOnGraph(airspaces));
-  }
-
-  shouldUpdate(changedProps: PropertyValues): boolean {
-    if (changedProps.has('timestamp')) {
-      if (this.thumbElement) {
-        const x = String(this.xForTimestamp());
-        this.thumbElement.setAttribute('x1', x);
-        this.thumbElement.setAttribute('x2', x);
-      }
-      changedProps.delete('timestamp');
-    }
-    return super.shouldUpdate(changedProps);
   }
 }
