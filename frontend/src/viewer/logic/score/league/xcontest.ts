f8cb5870b43a361516fa507393268224cf8856b9
Victor Berchet <victor@suumit.com>
2021-09-24 15:57:55 -0700

    Update Distanseligaen scoring Oct 1, 2021

--- frontend/src/viewer/logic/score/league/xcontest.ts
+++ frontend/src/viewer/logic/score/league/xcontest.ts
@@ -55,10 +55,10 @@ export class NorwayLeague {
   }
 
   protected scoreTriangles1: ScoreFunction = (measure: Measure, distances: number[], indexes: number[]): Score[] =>
-    scoreTriangles(2.0, 2.6, 0.05, false, true, measure, distances, indexes);
+    scoreTriangles(1.7, 2.4, 0.05, false, true, measure, distances, indexes);
 
   protected scoreTriangles2 = (measure: Measure, distances: number[], indexes: number[]): Score[] =>
-    scoreTriangles(1.8, 2.4, 0.2, false, true, measure, distances, indexes);
+    scoreTriangles(1.5, 2.2, 0.2, false, true, measure, distances, indexes);
 
   protected openDistanceMultiplier(): number {
     return 1;
