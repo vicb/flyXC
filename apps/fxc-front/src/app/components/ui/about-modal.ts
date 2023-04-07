682f3fd1a52e8b876363e629a421905ec2723368
Victor Berchet <victor@suumit.com>
2023-04-07 12:29:24 -0700

    Add credits

--- apps/fxc-front/src/app/components/ui/about-modal.ts
+++ apps/fxc-front/src/app/components/ui/about-modal.ts
@@ -33,6 +33,18 @@ export class AboutModal extends LitElement {
         <ion-text color="medium">
           <p><em>build ${BUILD}</em></p>
         </ion-text>
+
+        <ion-text>
+          <p>Credits:</p>
+          <ul class="credits">
+            <li>Airspace data from <a href="https://www.openaip.net/" target="_blank">openaip</a></li>
+            <li>Thermal data from <a href="https://thermal.kk7.ch/" target="_blank">Michael von Kanel</a></li>
+            <li><a href="https://js.arcgis.com/" target="_blank">JS Arcgis API</a></li>
+            <li><a href="https://ionicframework.com/" target="_blank">Ionic</a></li>
+            <li><a href="https://icons8.com/" target="_blank">Line Awesome</a></li>
+            <li>and many more open source libraries, projects, and art works</li>
+          </ul>
+        </ion-text>
       </ion-content>
       <ion-footer>
         <ion-toolbar color="light">
