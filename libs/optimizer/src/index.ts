960bae52a6c2e6baac6730606c6ce213a5f2d3c0
Christophe TARET <ch.taret@gmail.com>
2024-05-22 23:25:29 +0200

    refactor(optimizer): explicit API
    export required types in index.ts

--- libs/optimizer/src/index.ts
+++ libs/optimizer/src/index.ts
@@ -1,2 +1,2 @@
-export { optimize } from './lib/optimizer';
+export { optimize, OptimizationOptions, OptimizationRequest, OptimizationResult, ScoringTrack } from './lib/optimizer';
 export { LeagueCode } from './lib/scoringRules';
