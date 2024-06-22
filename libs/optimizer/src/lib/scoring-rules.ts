e9a859ee396cab83bc922874c62714a85a1a9c71
Karel Vrbensky <vrka@users.noreply.github.com>
2024-06-22 18:56:53 +0200

    Add correct closingDistanceRelative for Czech leagues (#254)

--- libs/optimizer/src/lib/scoring-rules.ts
+++ libs/optimizer/src/lib/scoring-rules.ts
@@ -9,22 +9,24 @@ const freeTriangle = scoringBaseModel[1];
 const faiTriangle = scoringBaseModel[2];
 const outAndReturn = igcXcScore.scoringRules['FAI-OAR'][0];
 
+//Czech rules see https://www.xcontest.org/cesko/pravidla/
+
 const czechLocalRule = [
   { ...openDistance, multiplier: 1 },
-  { ...freeTriangle, multiplier: 1.8 },
-  { ...faiTriangle, multiplier: 2.2 },
+  { ...freeTriangle, multiplier: 1.8, closingDistanceRelative: 0.05 },
+  { ...faiTriangle, multiplier: 2.2, closingDistanceRelative: 0.05 },
 ];
 
 const czechEuropeRule = [
   { ...openDistance, multiplier: 1 },
-  { ...freeTriangle, multiplier: 1.2 },
-  { ...faiTriangle, multiplier: 1.4 },
+  { ...freeTriangle, multiplier: 1.2, closingDistanceRelative: 0.05 },
+  { ...faiTriangle, multiplier: 1.4, closingDistanceRelative: 0.05 },
 ];
 
 const czechOutEuropeRule = [
   { ...openDistance, multiplier: 0.8 },
-  { ...freeTriangle, multiplier: 1.2 },
-  { ...faiTriangle, multiplier: 1.4 },
+  { ...freeTriangle, multiplier: 1.2, closingDistanceRelative: 0.05 },
+  { ...faiTriangle, multiplier: 1.4, closingDistanceRelative: 0.05 },
 ];
 
 const leonardoRule = [
