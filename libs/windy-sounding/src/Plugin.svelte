6d36d5b3b2bcff6319ccd35b38bad0dd42a47210
Victor Berchet <victor@suumit.com>
2024-07-13 14:42:13 +0200

    Add support for the former URL scheme

--- libs/windy-sounding/src/Plugin.svelte
+++ libs/windy-sounding/src/Plugin.svelte
@@ -5,9 +5,19 @@
   let pluginElement: HTMLElement;
 
   export const onopen = (parameters: any) => {
+    // Legacys URL do not have the model.
+    // old format /:lat/:lon
+    // new format /:model/:lat/:lon
+    const isNumeric = (value: string) => value as any == parseFloat(value);
+    if (isNumeric(parameters?.modelName) && isNumeric(parameters?.lat)) {
+      parameters.lon = parameters.lat;
+      parameters.lat = parameters.model;
+      parameters.model = 'ecmwf';
+    }
+
     const mapCenter = W.map.map.getCenter();
     const lat = Number(parameters?.lat ?? mapCenter.lat);
-    const lon = Number(parameters?.long ?? mapCenter.lng);
+    const lon = Number(parameters?.lon ?? mapCenter.lng);
     const modelName = parameters?.modelName ?? 'ecmwf';
     openPlugin({ lat, lon, modelName });
   };
