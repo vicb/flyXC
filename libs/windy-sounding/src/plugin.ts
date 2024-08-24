6c4a341271ad33957456eb0c2b62b066e3230218
Victor Berchet <victor@suumit.com>
2024-08-24 13:14:51 +0200

    Fix the deadline for the required update
    Check the time since the last release instead of the time between releases

--- libs/windy-sounding/src/plugin.ts
+++ libs/windy-sounding/src/plugin.ts
@@ -13,10 +13,9 @@ import Plugin from './Plugin.svelte';
 // Set the build timestamp on the dev server.
 if (process.env.NODE_ENV !== 'production') {
   (window as any).__BUILD_TIMESTAMP__ = Date.now();
-}
-
-if (!pluginConfig.name.startsWith('windy-plugin-')) {
-  throw new Error(`Plugin name "${pluginConfig.name}" should start with "windy-plugin-".`);
+  if (!pluginConfig.name.startsWith('windy-plugin-')) {
+    throw new Error(`Plugin name "${pluginConfig.name}" should start with "windy-plugin-".`);
+  }
 }
 
 export { pluginConfig as __pluginConfig, Plugin as default };
