cbf974ebecc05065671352191e0a5f2132f42acf
Victor Berchet <victor@suumit.com>
2025-01-06 10:17:43 +0100

    Misc fixes for airspaces

--- apps/fxc-tiles/src/assets/airspaces/download.sh
+++ apps/fxc-tiles/src/assets/airspaces/download.sh
@@ -6,7 +6,8 @@ DST_FOLDER=`realpath $DIST_ASP_FOLDER/../../../../apps/fxc-tiles/src/assets/airs
 echo "# Download openaip airspaces"
 node $DIST_ASP_FOLDER/download-openaip.js -o $DST_FOLDER
 
-echo "# Download Ukraine airspaces"
-mkdir -p /tmp/asp/
-wget -O /tmp/asp/ukraine.zip "https://fly.net.ua/airspaces/UKRAINE%20(UK).zip"
-unzip -o "/tmp/asp/ukraine.zip" -d $DST_FOLDER
+# no more available
+# echo "# Download Ukraine airspaces"
+# mkdir -p /tmp/asp/
+# wget -O /tmp/asp/ukraine.zip "https://fly.net.ua/airspaces/UKRAINE%20(UK).zip"
+# unzip -o "/tmp/asp/ukraine.zip" -d $DST_FOLDER
