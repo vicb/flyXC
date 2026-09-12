376537b67dc39202412aa99d2a0081e4370de71e
Victor Berchet <victor@suumit.com>
2026-09-12 07:29:13 +0200

    fix: a couple of minor fixes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    projects: ['**/*/vite.config.ts', '**/*/vitest.config.ts'],
+    projects: ['**/*/vite.config.ts', '**/*/vitest.config.ts', '!**/dist/workspace_modules/**'],
   },
 });
